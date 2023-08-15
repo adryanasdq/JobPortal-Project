4837b83961cc27c5da3915564c4c118816dfb5de
adryanasdq <adryan@upi.edu>
2023-08-15 15:27:32 +0700

    fixing return data

--- frontend/js/landing.js
+++ frontend/js/landing.js
@@ -42,16 +42,11 @@ async function login(e) {
     const response = await fetch('http://127.0.0.1:5000/login', requestOptions);
     const result = await response.json();
 
-    const encodedUsername = btoa(result.username);
-    const encodedPassword = btoa(result.password);
-
     try {
         const userStringId = result.id.toString();
         localStorage.setItem("isLoggedIn", true);
         localStorage.setItem("id", result.id);
         localStorage.setItem("name", result.name);
-        localStorage.setItem("username", encodedUsername);
-        localStorage.setItem("password", encodedPassword);
         if (userStringId[0] === '1') {
             window.location.href = "home-company.html";
         } else {
